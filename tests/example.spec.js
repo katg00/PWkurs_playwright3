dcbcb65ab9e4bb4056c542220993e71ad88405a3
katg00 <kat.kursy@gmail.com>
2026-03-11 19:27:35 +0100

    zmienne

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,9 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto('/');
+  await page.getByTestId('login-username').fill(process.env.USER);
+  await page.getByTestId('login-password').fill(process.env.PASSWORD);
+  await page.getByTestId('login-button').click();
+  await expect(page.getByTestId('welcome-msg')).toContainText(`Witaj: ${process.env.USER}`);
 });
